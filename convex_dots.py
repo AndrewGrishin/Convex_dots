0930f092c243516af9c646aec399ef524d2bf618
FelixMonteg <FelixMonteg@users.github.com>
2020-10-25 01:38:24 +0300

    new features

--- convex_dots.py
+++ convex_dots.py
@@ -1,4 +1,7 @@
-def distanceFromCenter(dot, centre):
+import matplotlib.pyplot as plt
+import numpy as np
+
+def distance(dot, centre):
     return ((dot[0] - centre[0])**2 + ((dot[1] - centre[1])**2))**(0.5)
 
 file = open("input_dots.txt","r")
@@ -8,62 +11,37 @@ del(file)
 
 x = [i[0] for i in dots]
 y = [i[1] for i in dots]
-centre = ((sum(x) / len(x), sum(y) / len(y)),0)
+centre = (sum(x) / len(x), sum(y) / len(y))
 del(x)
 del(y)
 
-dots = list(map(lambda dot: (dot, distanceFromCenter(dot,centre[0])),dots))
-dots = sorted(dots, key = lambda dot : dot[1], reverse = 1)
+dots = list(map(lambda dot: dot,dots))
+dots = sorted(dots, key = lambda dot : distance(dot, centre), reverse = 1)
 
 def ru_er(dot, centre):
-    return dot[0][0] >= centre[0][0] and dot[0][1] >= centre[0][1]
+    return dot[0] >= centre[0] and dot[1] >= centre[1]
 def rd_er(dot, centre):
-    return dot[0][0] >= centre[0][0] and dot[0][1] <= centre[0][1]
+    return dot[0] >= centre[0] and dot[1] <= centre[1]
 def lu_er(dot, centre):
-    return dot[0][0] <= centre[0][0] and dot[0][1] >= centre[0][1]
+    return dot[0] <= centre[0] and dot[1] >= centre[1]
 def ld_er(dot, centre):
-    return dot[0][0] <= centre[0][0] and dot[0][1] <= centre[0][1]
+    return dot[0] <= centre[0] and dot[1] <= centre[1]
+def sorter(array,dot, func):
+        if dot not in array and sum(1 for sub_dot in array if func(sub_dot,dot)) == 0:
+            return 1
 
-ru = []
-rd = []
-lu = []
-ld = []
+dotsMatrix = [[],[],[],[]]
 
 for dot in dots:
-    if ru_er(dot,centre):
-        if dot not in ru:
-            k = 0
-            for sub_dot in ru:
-                if ru_er(sub_dot,dot):
-                    k += 1
-            if k == 0:
-                ru.append(dot)
-    elif rd_er(dot,centre):
-         if dot not in rd:
-            k = 0
-            for sub_dot in rd:
-                if rd_er(sub_dot,dot):
-                    k += 1
-            if k == 0:
-                rd.append(dot)
-    elif ld_er(dot,centre):
-        if dot not in ld:
-            k = 0
-            for sub_dot in ld:
-                if ld_er(sub_dot,dot):
-                    k += 1
-            if k == 0:
-                ld.append(dot)
+    if ru_er(dot,centre) and sorter(dotsMatrix[0],dot,func = ru_er):
+        dotsMatrix[0].append(dot)
+    elif rd_er(dot,centre) and sorter(dotsMatrix[1],dot,func = rd_er):
+        dotsMatrix[1].append(dot)
+    elif ld_er(dot,centre) and sorter(dotsMatrix[2],dot,func = ld_er):
+        dotsMatrix[2].append(dot)
     else:
-        if dot not in lu:
-            k = 0
-            for sub_dot in lu:
-                if lu_er(sub_dot,dot):
-                    k += 1
-            if k == 0:
-                lu.append(dot)
+        if sorter(dotsMatrix[3],dot,func = lu_er):
+            dotsMatrix[3].append(dot)
 
-print("Right-Up",ru)
-print("Right-Down",rd)
-print("Left-Down",ld)
-print("Left-Up",lu)
+dotsMatrix = [j for i in dotsMatrix for j in i]
+# do NOT flat and sort in each sector!!!
